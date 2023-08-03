fd18d2b43995740cc79108441c9debc8dc0eccc3
sw59 <sw59@njit.edu>
2023-08-03 14:27:58 +0800

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component class="h-full bg-no-repeat bg-cover bg-center bg-[url('/e2AY3k8EYn.png')]"  {...pageProps} />
+  return <Component  {...pageProps} />
 }
