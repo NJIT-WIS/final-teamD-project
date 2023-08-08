d2a7d4e25376ef7e34482e3d8efed8e6cfa9d00c
sw59 <sw59@njit.edu>
2023-08-08 20:31:28 +0800

    contact page

--- src/pages/ContactPage/index.js
+++ src/pages/ContactPage/index.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react'
-
+import { DribbbleOutlined, WhatsAppOutlined, TwitterOutlined, LinkedinOutlined, MailOutlined } from '@ant-design/icons';
 class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
       contentList: [
-        "Peet's Coffee: Peet's Coffee is an artisanal with a focus on quality and sustainability, offering a unique coffee experience to a community of enthusiasts who value the art of coffee-making and itspositive impact on the environment.",
-        "Dunkin Donuts: Dunkin Donuts is a globally recognized brand, delighting customers with a wide array of freshly made donuts and exceptional coffee, providing convenient and friendly atmosphere for people of all walks of life to enjoy their favorite treats.",
-        "Starbucks: Starbucks, the magical coffee destination, aims to inspire and nurture individuals, creating meaningful connections through their high-quality coffee offerings, while making a positive impact in the world through community-driven initiatives and social responsibility."
+
       ]
     };
   }
+
+
+
   render() {
     return (
-      <div className="bg-[url('/e2AY3k8EYn.png')] bg-cover bg-center bg-no-repeat h-full flex flex-col justify-center">
-        {
-          this.state.contentList.map((item, index) => {
-            return (
-              <>
-                <span className=' whitespace-pre-line text-indent ml-6 w-1/2 font-bold text-[#ffffff] text-3xl text-justify'>
-                  {item}
-                </span>
-                <br></br>
-              </>
+      <div className="bg-[url('/e2AY3k8EYn.png')] bg-cover bg-center bg-no-repeat h-full flex flex-col ">
+        <div className='ml-6'>
+          <div className='text-[100px] hover:text-[105px]' >
+            <DribbbleOutlined className='hover:cursor-pointer ' />
+          </div>
+          <div className='text-[100px] hover:text-[105px]'>
+            <WhatsAppOutlined className='hover:cursor-pointer ' />
+          </div>
+          <div className='text-[100px] hover:text-[105px]'>
+            <TwitterOutlined className='hover:cursor-pointer ' />
+          </div>
+          <div className='text-[100px] hover:text-[105px]'>
+            <MailOutlined className='hover:cursor-pointer ' />
+          </div>
+          <div className='text-[100px] hover:text-[105px]'>
+            <LinkedinOutlined className='hover:cursor-pointer ' />
+          </div>
+        </div>
 
-            )
-          })
-        }
       </div>
     );
   }
